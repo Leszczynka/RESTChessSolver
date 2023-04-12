3117665713d847747bb8cea658aa3068066cc366
leszczynka <karolina.leszczynska05@gmail.com>
2023-04-12 18:32:07 +0200

    Create a method to get figure position indexes on the board

--- models.py
+++ models.py
@@ -1,7 +1,12 @@
 from abc import ABC, abstractmethod
 
+BOARD = [[letter+number for letter in 'ABCDEFGH'] for number in '12345678']
+
 
 class Figure(ABC):
+    row_index: int
+    col_index: int
+
     def __init__(self, position: str):
         self.position = position
 
@@ -11,6 +16,10 @@ class Figure(ABC):
 
     def validate_move(self, dest_field: str) -> bool:
         pass
+
+    def get_index_from_the_board(self):
+        self.row_index = [BOARD.index(row) for row in BOARD if self.position in row][0]
+        self.col_index = [row.index(self.position) for row in BOARD if self.position in row][0]
 
 
 class King(Figure):
@@ -42,3 +51,9 @@ class Pawn(Figure):
     def list_available_moves(self) -> list[str]:
         pass
 
+
+print(BOARD)
+k = King("C3")
+k.get_index_from_the_board()
+print(k.row_index)
+print(k.col_index)
