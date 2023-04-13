5f2cd6ba49e8feb38470f64b67ec7f1ea722ede3
leszczynka <karolina.leszczynska05@gmail.com>
2023-04-13 19:14:01 +0200

    Create an endpoint that validates move

--- app.py
+++ app.py
@@ -33,6 +33,43 @@ def get_available_moves(chess_figure: str, current_field: str):
     ), response_code
 
 
+@app.route('/api/v1/<string:chess_figure>/<string:current_field>/<string:dest_field>', methods=['GET'])
+def validate_move(chess_figure: str, current_field: str, dest_field: str):
+    error = None
+    response_code = None
+    move = 'invalid'
+    c_field = verify_field(current_field)
+    d_field = verify_field(dest_field)
+    figure = verify_figure(chess_figure)
+    if not c_field or not d_field:
+        error = 'Field does not exist.'
+        response_code = 409
+    elif not figure:
+        error = 'Figure does not exist.'
+        response_code = 404
+    elif c_field and d_field and figure:
+        move = figure(current_field.upper()).validate_move(dest_field.upper())
+        if move:
+            move = 'valid'
+            error = 'null'
+
+        else:
+            move = 'invalid'
+            error = 'Current move is not permitted.'
+
+        response_code = 200
+
+    return jsonify(
+        {
+            "move": move,
+            "error": error,
+            "figure": chess_figure.lower(),
+            "currentField": current_field.upper(),
+            "destField": dest_field.upper(),
+        }
+    ), response_code
+
+
 def verify_figure(chess_figure: str):
     match chess_figure.capitalize():
         case 'King':
