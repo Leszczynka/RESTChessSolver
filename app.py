f1faa1c1e880c3d4b9726243ee8a63ad29148000
leszczynka <karolina.leszczynska05@gmail.com>
2023-04-14 09:57:40 +0200

    Refactoring

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from typing import List
 from models import King, Queen, Rook, Knight, Bishop, Pawn
 
 
@@ -7,9 +8,8 @@ app = Flask(__name__)
 
 @app.route('/api/v1/<string:chess_figure>/<string:current_field>', methods=['GET'])
 def get_available_moves(chess_figure: str, current_field: str):
-    moves = []
-    error = None
-    response_code = None
+    moves: List[str] = []
+    current_field = current_field.upper()
     field = verify_field(current_field)
     figure = verify_figure(chess_figure)
     if not field:
@@ -18,8 +18,8 @@ def get_available_moves(chess_figure: str, current_field: str):
     elif not figure:
         error = 'Figure does not exist.'
         response_code = 404
-    elif field and figure:
-        moves = figure(current_field.upper()).list_available_moves()
+    else:
+        moves = figure(current_field).list_available_moves()
         error = 'null'
         response_code = 200
 
@@ -28,44 +28,41 @@ def get_available_moves(chess_figure: str, current_field: str):
             "availableMoves": moves,
             "error": error,
             "figure": chess_figure.lower(),
-            "currentField": current_field.upper()
+            "currentField": current_field
         }
     ), response_code
 
 
 @app.route('/api/v1/<string:chess_figure>/<string:current_field>/<string:dest_field>', methods=['GET'])
 def validate_move(chess_figure: str, current_field: str, dest_field: str):
-    error = None
-    response_code = None
     move = 'invalid'
-    c_field = verify_field(current_field)
-    d_field = verify_field(dest_field)
     figure = verify_figure(chess_figure)
-    if not c_field or not d_field:
+    current_field = current_field.upper()
+    dest_field = dest_field.upper()
+
+    if not verify_field(current_field) or not verify_field(dest_field):
         error = 'Field does not exist.'
         response_code = 409
     elif not figure:
         error = 'Figure does not exist.'
         response_code = 404
-    elif c_field and d_field and figure:
-        move = figure(current_field.upper()).validate_move(dest_field.upper())
-        if move:
-            move = 'valid'
-            error = 'null'
-
-        else:
-            move = 'invalid'
-            error = 'Current move is not permitted.'
-
+    else:
+        move = figure(current_field).validate_move(dest_field)
         response_code = 200
+        if move:
+            error = 'null'
+            move = 'valid'
+        else:
+            error = 'Current move is not permitted.'
+            move = 'invalid'
 
     return jsonify(
         {
             "move": move,
             "error": error,
             "figure": chess_figure.lower(),
-            "currentField": current_field.upper(),
-            "destField": dest_field.upper(),
+            "currentField": current_field,
+            "destField": dest_field,
         }
     ), response_code
 
@@ -85,12 +82,12 @@ def verify_figure(chess_figure: str):
         case 'Pawn':
             figure = Pawn
         case _:
-            figure = False
+            figure = None
     return figure
 
 
-def verify_field(field: str):
-    return field[0].upper() in 'ABCDEFGH' and field[1] in '12345678' and len(field) == 2
+def verify_field(field: str) -> bool:
+    return field[0] in 'ABCDEFGH' and field[1] in '12345678' and len(field) == 2
 
 
 if __name__ == '__main__':
